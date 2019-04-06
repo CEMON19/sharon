2ab3548cddb5b5cb90559af86feee303b1c5c930
CEMON19 <49330945+CEMON19@users.noreply.github.com>
2019-04-06 12:37:14 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -14,16 +14,11 @@ int add2nos(int a,int b,int c)
  {
      printf("%d+%d=%d",a,b,c);
  }    
-void main()
+int main()
 {
-    int a,b,c;
-    printf("enter number 1\n");
-    scanf("%d",&a);
-    printf("enter number 2\n");
-    scanf("%d",&b);
-    c=a+b;
-    printf("%d+%d=%d",a,b,c);
-    
+    get_input(int a,int b,int *c);
+    compute(a,b,&c);
+    get_output(int a,int b,int c);
+    return 0;
 }
-
 
