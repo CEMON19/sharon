e2cc8358bc570dfd31f4bf0274d6327f5592b968
CEMON19 <49330945+CEMON19@users.noreply.github.com>
2019-04-06 12:17:31 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,7 +6,24 @@ void get_input(int *a,int *b)
     printf("enter number 2\n");
     scanf("%d",b);
 }
-int add2nos(int a,int b)
+int add2nos(int a,int b,int c)
 {
-
+    c=a+b;
 }
+ void get_output(int a,int b,int c)
+ {
+     printf("%d+%d=%d",a,b,c);
+ }    
+void main()
+{
+    int a,b,c;
+    printf("enter number 1\n");
+    scanf("%d",&a);
+    printf("enter number 2\n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("%d+%d=%d",a,b,c);
+    
+}
+
+
