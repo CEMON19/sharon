6f0e3244a4ecac2488cd3445307aabf37265648d
CEMON19 <49330945+CEMON19@users.noreply.github.com>
2019-04-06 12:02:43 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void get_input(int *a,int *b)
+{
+    printf("enter number 1\n");
+    scanf("%d",a);
+    printf("enter number 2\n");
+    scanf("%d",b);
+}
+int add2nos(int a,int b)
+{
+
+}
