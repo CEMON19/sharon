2fc6992e7bf278a2497870bf6800cba130dfbdcc
CEMON19 <49330945+CEMON19@users.noreply.github.com>
2019-04-07 12:50:02 +0530

    Create compare.c

---
+++ compare.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void get_input(int *a,int *b,int *c)
+{
+    printf("enter number 1\n");
+    scanf("%d",a);
+    printf("enter number 2\n");
+    scanf("%d",b);
+    printf("enter number 3\n");
+    scanf("%d",c);
+}
+int compare(int a,int b,int c)
+{
+    if (a>b)
+    {
+        if (a<c)
+        printf("smallest number is %d",a);
+    }
+    if (b<c)
+    {
+        if(b<a)
+        printf("smallest number is %d",b);
+    }
+    if(c<a)
+    {
+        if(c<b)
+        printf("smallest number is %d",c);
+    }
+}
