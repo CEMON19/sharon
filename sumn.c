293da2393f565de049d43ce48e42435d48a900be
CEMON19 <49330945+CEMON19@users.noreply.github.com>
2019-04-07 11:39:53 +0530

    Update sumn.c

--- sumn.c
+++ sumn.c
@@ -8,16 +8,20 @@ void get_input(int *n,int a[n])
 { 
     int i;
     for( i=0;i<*n;i++)
+    {
     printf("enter next number\n");
     scanf("%d",&a[i]);
+    }
 }
 int addn(int *n,int sum,int a[n])
 {
     int i;
     sum=0;
     for( i=0;i<*n;i++)
+    {
     sum+=a[i];
-   }
+    }
+ }
 void get_output(int sum)
 {
     printf("sum=%d",sum);
@@ -27,8 +31,9 @@ int main()
     int n,i,a[n],sum;
     getn(&n);
     get_input(&n,a[n]);
-    addn(n,sum,i,a[n]);
+    addn(&n,sum,a[n]);
     get_output(sum);
     return 0;
 }
+    
     
